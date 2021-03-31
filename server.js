eb484bfd91c0643079dd7461f79dcd58adfe1c94
JaredWeav <jared.weaver2@gmail.com>
2021-03-31 12:56:46 -0400

    added routes/models

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
